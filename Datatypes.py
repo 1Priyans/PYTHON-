8eb165893b3b8d1ec573411ad955b260d0fbfc0f
pranjal00 <pranjalgoodguy@gmail.com>
2023-05-08 13:05:39 +0530

    Conversions of datatypes in int

---
+++ Datatypes.py
@@ -0,0 +1,39 @@
+#Conversion from Decimal numer to Binary number system values
+a = 1223323232123
+print(bin(a))
+
+#Conversion from Octal numer to Binary number system values
+a = 0o27
+print(bin(a))
+
+#Conversion from Hexadecimal numer to Binary number system values
+a = 0x159
+print(bin(a))
+
+a = 0xAF
+print(bin(a))
+
+#Conversion from Decimal numer to Octal number system values
+a = 1234
+print(oct(a))
+
+#Conversion from Binary numer to Octal number system values
+a = 0b01
+print(oct(a))
+
+#Conversion from Hexa Decimal numer to Octal number system values
+a = 0x183450
+print(oct(a))
+
+#Conversion from Decimal numer to Heaxadecimal number system values
+a = 378298749834232498
+print(hex(a))
+
+#Conversion from Binary numer to Hexadecimal number system values
+a = 0b0101101010100101010101010100110011001
+print(hex(a))
+
+#Conversion from Octal numer to HexaDecimal number system values
+a = 0o32424234267
+print(hex(a))
+
