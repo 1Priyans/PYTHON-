ae126dfb5e3c7669f88db26acfb457a1042b1c28
pranjal00 <pranjalgoodguy@gmail.com>
2023-05-10 12:16:28 +0530

    complex_datatypes

---
+++ float_DataType.py
@@ -0,0 +1,9 @@
+a = 50e4 #Means 50*10 to the power 4 here e is exponent i.e. e = 10
+print(a, type(a)) #Getting output a floating point value
+
+a = 5e-10000000000 #Means 5*10 to the power -100000000000 here e is exponent i.e. e = 10
+print(a, type(a)) #Getting output a floating point value
+
+# a = 0b010101010.0o21314234 #SyntaxError: invalid decimal literal
+# print(a,type(a)) #float data does not support binary, octal, hexa decimal number system values to store float number values.
+
