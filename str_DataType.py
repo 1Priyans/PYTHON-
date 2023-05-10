b8d80143f29e393d78eb51116a661809906e73e6
pranjal00 <pranjalgoodguy@gmail.com>
2023-05-10 15:50:01 +0530

    str_Datatype

---
+++ str_DataType.py
@@ -0,0 +1,13 @@
+#With the help double Quotes ("  ") and single Quotes (' ') we can store single line str data only but
+#not possible to store multi line string data.
+a = "Guido van Russom"
+print(a[0])
+
+#With the help tripple double Quotes (" " "     " " ") and Tripple single Quotes (' ' '    ' ' ') 
+# we can store single line str data and multi line string data.
+information = '''Guido van Rossum
+... HNO:3-4,Hill Side
+... Python Software Foundation
+... Nether Lands-56'''
+print(information,type(information))
+
