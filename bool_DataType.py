3a52a21366d136c4a3956226e68d222754343b13
pranjal00 <pranjalgoodguy@gmail.com>
2023-05-10 14:04:25 +0530

    bool_Datatypes

---
+++ bool_DataType.py
@@ -0,0 +1,8 @@
+#Internally True is Considered as 1 and False is Considered as 0
+#True and False are the Keywords and They are treated as Boolean values.
+a=True 
+b=False
+c=a+b
+print(a,type(a))
+print(2*True+3*False) 
+print(0xA*True-True) #here, A is Hexa Decimal number system value i.e. A = 10
