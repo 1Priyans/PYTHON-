6fcaaf7196da1fc7718ae834b73ec9fe2ad5ac1c
pranjal00 <pranjalgoodguy@gmail.com>
2023-05-24 00:25:13 +0530

    NoneType data type

---
+++ DataTypes_in_Python.py/nonetype_DataType/nonetype_DataType.py
@@ -0,0 +1,15 @@
+___Properties = '''1) 'NoneType' is one the pre-defined class and treated as None type Data type
+
+2) "None" is keyword acts as value for <class,'NoneType'>
+
+3)The value of 'None' is not False, Space , empty  , 0 
+
+4)An object of NoneType class can't be created explicitly.'''
+
+#Example:
+a = None
+print(a,type(a)) # None <class 'NoneType'>
+# b = list(a)
+# print(a,type(a)) # TypeError: 'NoneType' object is not iterable
+
+# print(len(a)) # TypeError: object of type 'NoneType' has no len()
