51e0b241e92b95b348afe11d4171a165c6d7da56
pranjal00 <pranjalgoodguy@gmail.com>
2023-05-28 00:54:38 +0530

    Operators and Expression in Python

---
+++ Operators  and Expressions in Python/Operators_and_Expressions_in_Python.py
@@ -0,0 +1,27 @@
+__Properties = '''1) An Operator is a symbol, which is used for Performing Certain Operation on given Object Data / Values and 
+                    gives Result.
+
+2) If two or More Objects OR Values Connected with an Operator then it is called Expression.
+
+3)In other words, An Expression is a collection of Objects / Values Connected with an Operator.
+
+In Python Programming, we have 7 Operators. They are
+
+1. Arithmetic Operators
+2. Assignment Operator
+3. Relational Operators ( Comparison Operators)
+4. Logical Operators(  Comparison Operators )
+5. Bitwise Operators (Most Imp)
+6. Membership Operators
+                        a) in operator
+                        b) not in operator
+7. Identity Operators
+				        a) is operator
+				        b) is not operator '''
+
+
+
+
+
+
+
