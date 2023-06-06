ab23031bfd9af8737bc0257e6571f074eec44224
pranjal00 <pranjalgoodguy@gmail.com>
2023-06-06 13:55:27 +0530

    Looping Statement

---
+++ Flow Control Statements in Python/Looping OR Iterative Or Repetative Statements/Looping OR Iterative Or Repetative Statements.py
@@ -0,0 +1,13 @@
+__Properties = '''The purpose of Looping OR Iterative Or Repetative Statements is that " To Perform certain Operation Repeatedly OR 
+                  Iteratively for Finite Number of Times until Test Condition becomes False."
+
+In Python Programming, we have 2 types of Lopping Statements. They are
+
+    1. while loop   OR  while..else loop
+    2. for loop   OR   for ..else Loop
+
+In Order to deal with Looping Statements, Programmer Must ensure that There must exist 3 points. They are
+    a) Initlization part ( Where to start)
+    b) Conditional Part ( Where to Stop)
+	c) Updation Part  ( How much to increment / Decrement) '''
+
