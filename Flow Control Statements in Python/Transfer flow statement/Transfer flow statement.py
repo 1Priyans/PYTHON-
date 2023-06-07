b709d7e5411fed190c4bc3b270687b8746ca4ef8
pranjal00 <pranjalgoodguy@gmail.com>
2023-06-07 16:02:28 +0530

    Transfer Flow Statement

---
+++ Flow Control Statements in Python/Transfer flow statement/Transfer flow statement.py
@@ -0,0 +1,11 @@
+# Transfer Flow Control Statements
+__Properties = '''
+The purpose of Transfer Flow Control Statements in python is that " To transfer the control of PVM 
+    from one part of the program to another part of the program".
+
+In Python Programming, we have 4 types of Transfer Flow Control Statements. They are
+
+1. break
+2. continue
+3. pass
+4. return '''
